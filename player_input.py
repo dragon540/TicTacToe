c91b01e6d0cabb470b8b5d78e3a329def8c74547
Shobhit <shobhit32t@gmail.com>
2023-03-06 15:07:00 +0530

    fixed indentation

--- player_input.py
+++ player_input.py
@@ -3,13 +3,13 @@
 import Game_var
 
 
-def playerA_Turn() :
+def playerA_Turn():
     row = int(input("PLAYER A \nEnter row of board "))
     column = int(input("Enter column of board "))
 
     # add checks for wrong input
     # do something to stop player from changing the value of a location which is not empty
-    
+
     Game_var.board[row][column] = "X"
 
 
