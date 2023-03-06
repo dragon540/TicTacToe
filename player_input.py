8f34d40880772467a18271b93eb885e2f26d8130
Shobhit <shobhit32t@gmail.com>
2023-03-06 14:12:13 +0530

    added todo comment for later

--- player_input.py
+++ player_input.py
@@ -7,11 +7,17 @@ def playerA_Turn() :
     row = int(input("PLAYER A \nEnter row of board "))
     column = int(input("Enter column of board "))
 
+    # add checks for wrong input
+    # do something to stop player from changing the value of a location which is not empty
+    
     Game_var.board[row][column] = "X"
 
 
 def playerB_Turn():
     row = int(input("PLAYER B \nEnter row of board "))
     column = int(input("Enter column of board "))
+
+    # add checks for wrong input
+    # do something to stop player from changing the value of a location which is not empty
 
     Game_var.board[row][column] = "O"
